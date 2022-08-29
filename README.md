d77dbc6f597a223c1c3a74133112a270db14ab73
Huy pham <phamquangh@gmail.com>
2022-08-29 14:26:27 +1000

    Improve export

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ To find and export most recent tweets:
 
 1. Install [Node and NPM](https://nodejs.org/en/download/)
 1. Execute `npm run install` to install all required dependencies
-1. Run `npm run start -- [access token] [search query] [number of tweets limit]` (e.g `npm run generate -- ADSALKDJ javascript 100`)
+1. Run `npm run start -- [access token] [search query] [number of tweets limit]` (e.g `npm run start -- ADSALKDJ javascript 100`)
--- twitter.js
+++ twitter.js
@@ -1,9 +1,12 @@
 const { TwitterApi } = require('twitter-api-v2');
 const XLSX = require('XLSX');
+const short = require('short-uuid');
+
+const shortUUID = short.generate();
 
 const ACCESS_TOKEN = process.argv[2];
 const SEARCH_QUERY = process.argv[3];
-const LIMIT = process.argv[4] || 1000;
+const LIMIT = process.argv[4] || 100;
 
 // Instantiate with desired auth type (here's Bearer v2 auth)
 const twitterClient = new TwitterApi(ACCESS_TOKEN);
@@ -13,7 +16,9 @@ const jsonToXLSX = (data) => {
   const workbook = XLSX.utils.book_new();
   XLSX.utils.book_append_sheet(workbook, worksheet, "Tweets");
 
-  XLSX.writeFile(workbook, "Report.xlsx");
+  const fileName = `report_auto_${shortUUID}.xlsx`;
+  XLSX.writeFile(workbook, fileName);
+  console.log(`Exported ${data.length} tweets to "${fileName}".`);
 }
 
 const exportTweets = async () => {
@@ -23,7 +28,7 @@ const exportTweets = async () => {
   // Consume every possible tweet of jsTweets (until rate limit is hit)
   for await (const tweet of jsTweets) {
     tweets.push(tweet);
-    if (tweets.length > LIMIT) break;
+    if (tweets.length >= LIMIT) break;
   }
 
   jsonToXLSX(tweets);
